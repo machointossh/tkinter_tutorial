8fef434e5a09bbf70d9e36a6a8360aa65fb268ac
hiphopku <tosh.tanaka.biz@gmail.com>
2019-05-24 20:37:37 +0900

    Added New Window (Not Finished Yet)

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 import tkinter as tk
 
+
 LARGE_FONT = ("Verdana", 12)
+
 
 class SeaofBTCapp(tk.Tk):
 
     def __init__(self, *args, **kwargs):
 
-        super().__init__(self, *args, **kwargs)
+        # In the Part 2 Video (https://www.youtube.com/watch?v=A0gaXfM1UN0&list=PLQVvvaa0QuDclKx-QpC9wntnURXVJqLyk&index=2)
+        # tk.Tk.__init__(self, *args, **kwargs)
+        super().__init__()
 
         # Container (Boss of Each Frame)
         container = tk.Frame(self)
@@ -23,7 +27,7 @@ class SeaofBTCapp(tk.Tk):
         frame.grid(row=0, column=0, sticky=tk.NSEW)
         self.show_frame(StartPage)
 
-
+    # Show Up Frame
     def show_frame(self, cont):
 
         frame = self.frames[cont]
@@ -32,6 +36,33 @@ class SeaofBTCapp(tk.Tk):
 
 class StartPage(tk.Frame):
 
-    def __init__(self, parent, contoller):
+    def __init__(self, parent, controller):
+
+        # In the Part 2 Video (https://www.youtube.com/watch?v=A0gaXfM1UN0&list=PLQVvvaa0QuDclKx-QpC9wntnURXVJqLyk&index=2)
+        # super().__init__(self, parent)
+        super().__init__(parent)
+
+        label = tk.Label(self, text="Start Page", font=LARGE_FONT)
+        label.pack(padx=10, pady=10)
+
+        button = tk.Button(self, text="Visit Page 1",
+                           command=lambda: controller.show_frame(PageOne))
+        button.pack()
+
+
+class PageOne(tk.Frame):
+
+    def __init__(self, parent, controller):
 
         super().__init__(self, parent)
+        label = tk.Label(self, text="Back to Home", font=LARGE_FONT)
+        label.pack(padx=10, pady=10)
+        button1 = tk.Button(self, text="Visit Page 1",
+                            command=lambda: controller.show_frame(StartPage))
+        button1.pack()
+
+
+
+if __name__ == "__main__":
+    app = SeaofBTCapp()
+    app.mainloop()
